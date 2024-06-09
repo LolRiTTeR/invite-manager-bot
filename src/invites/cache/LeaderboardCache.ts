1b438f35cd6b53b1edff149c115ec619baf5bbe1
Alan <67201025+Alan-Wen-3344@users.noreply.github.com>
2024-06-10 00:50:34 +0800

    Add files via upload

--- src/invites/cache/LeaderboardCache.ts
+++ src/invites/cache/LeaderboardCache.ts
@@ -8,12 +8,12 @@ export class LeaderboardCache extends Cache<LeaderboardEntry[]> {
 	public async init() {
 		this.maxCacheDuration =
 			this.client.type === BotType.custom
-				? moment.duration(5, 'minute')
+				? moment.duration(3, 'minute')
 				: this.client.type === BotType.pro
-				? // ? moment.duration(1, 'hour')
-				  moment.duration(5, 'minute')
-				: // : moment.duration(24, 'hour');
-				  moment.duration(5, 'minute');
+				// ? moment.duration(1, 'hour')
+				? moment.duration(3, 'minute')
+				// : moment.duration(24, 'hour');
+				: moment.duration(5, 'minute');
 	}
 
 	protected async _get(guildId: string): Promise<LeaderboardEntry[]> {
